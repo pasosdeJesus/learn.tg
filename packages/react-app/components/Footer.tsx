aa2f0454142d91355bdda5083b9c2e080b5798a8
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-06-13 09:29:17 -0500

    localization

--- packages/react-app/components/Footer.tsx
+++ packages/react-app/components/Footer.tsx
@@ -6,9 +6,16 @@ export default function Footer({lang = "en"}) {
   return (
     <footer className="w-full bg-primary-200 z-20 dark:text-gray-900 flex flex-col px-16">
       <div className="flex flex-col justify-between lg:flex-row">
-        <div className="pt-4">
-          Unete a <a href="https://t.me/learn_t_g" className="hover:underline text-secondary-100 font-bold">la comunidad en Telegram</a>
-        </div>
+        { lang == "es" &&
+          <div className="pt-4">
+            Unete a <a href="https://t.me/learn_t_g" className="hover:underline text-secondary-100 font-bold">la comunidad en Telegram</a> 
+          </div>
+        }
+        { lang != "es" &&
+          <div className="pt-4">
+            Join <a href="https://t.me/learn_t_g" className="hover:underline text-secondary-100 font-bold">the community in Telegram</a> 
+          </div>
+        }
         <div className="flex flex-col justify-center pt-6 lg:pt-0">
           <div className="flex justify-center space-x-4">
             <a target="_blank" href="https://twitter.com/pasosdeJesus" className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-violet-600 dark:text-gray-50">
@@ -35,29 +42,20 @@ export default function Footer({lang = "en"}) {
               </svg>
             </a>
             <a target="_blank" href="https://github.com/pasosdeJesus" rel="norreferer" className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-violet-600 dark:text-gray-50">
-    <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="currentColor"
-    className="bi bi-github w-4 h-4"
-    viewBox="0 0 16 16"
-    >
-    <path
-    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"
-    />
-    </svg>
-    </a>
-    </div>
-    </div>
-    </div>
-
-    <div className="grid place-items-center w-full">
-    <p
-    className="md:text-lg font-medium text-center text-white mt-4 tracking-wider"
-    >
-    Puedes reusar abiertamente el contenido gratuito dando crédito.
-      {anioActual}
-    </p>
-    </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                className="bi bi-github w-4 h-4"
+                viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"
+                  />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 //          <p>Developed by <a target="_blank" href="https://www.pasosdeJesus.org">Pasos de Jesús</a> • Contact <a target="_blank" href="https://t.me/soporte_pdJ_bot">support</a></p>
