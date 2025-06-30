e60d40f5e0e7e55fe1baa09e09c5994fc595772b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-06-30 14:56:06 -0500

    space

--- packages/react-app/components/Layout.tsx
+++ packages/react-app/components/Layout.tsx
@@ -20,7 +20,7 @@ const Layout: FC<Props> = ({ children }) => {
     <>
      <div className="bg-gypsum overflow-hidden flex flex-col min-h-screen">
        <Header lang={lang} />
-       <div>
+       <div className="mt-8">
          {children}
        </div>
        <Footer lang={lang} />
