24fc9d18dd1dd456c0be50f1ea3146b3ce234469
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-07-21 10:45:34 -0500

    Updates to rails8. Closes #14

--- servidor/app/assets/config/manifest.js
+++ servidor/app/assets/config/manifest.js
@@ -1,7 +1,2 @@
 //= link_tree ../images
 //= link_tree ../builds
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
-//= link application.css
-//
-//= link recursos_sprockets.js
