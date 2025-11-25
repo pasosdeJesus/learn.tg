b1acd8234b34a592723ca55712ade6c449280288
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-11-25 04:39:24 -0500

    Advances #41

--- apps/nextjs/db/migrations/20251121001223_completed_guides_courses.ts
+++ apps/nextjs/db/migrations/20251121001223_completed_guides_courses.ts
@@ -2,17 +2,16 @@ import type { Kysely } from 'kysely'
 
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
-  .createTable('usuario_guidepaid')
+  .createTable('guidepaid_usuario')
   .addColumn('usuario_id', 'integer', (col) => col.notNull())
-  .addColumn('proyectofinanciero_id', 'integer', (col) => col.notNull())
-  .addColumn('actividadml_id', 'integer', (col) => col.notNull())
+  .addColumn('actividadpf_id', 'integer', (col) => col.notNull())
   .addColumn('amountpaid', 'integer', (col) => col.notNull())
   .addColumn('profilescore', 'integer', (col) => col.notNull())
-  .addColumn('amount_pending', 'integer', (col) => col.notNull())
+  .addColumn('amountpending', 'integer', (col) => col.notNull())
   .addColumn('points', 'integer', (col) => col.notNull())
   .execute();
   await db.schema
-  .createTable('usuario_coursecompleted')
+  .createTable('coursecompleted_usuario')
   .addColumn('usuario_id', 'integer', (col) => col.notNull())
   .addColumn('proyectofinanciero_id', 'integer', (col) => col.notNull())
   .addColumn('points', 'integer', (col) => col.notNull())
@@ -21,6 +20,6 @@ export async function up(db: Kysely<any>): Promise<void> {
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await db.schema.dropTable('usuario_coursecompleted').execute()
-  await db.schema.dropTable('usuario_guidepaid').execute()
+  await db.schema.dropTable('coursecompleted_usuario').execute()
+  await db.schema.dropTable('guidepaid_usuario').execute()
 }
