2c29ed4ee1816e713587fc36558d2633728de6d0
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2026-01-04 17:21:04 -0500

    better created_at

--- apps/nextjs/db/migrations/20260101203000_alter_userevent_usuario_id_nullable.ts
+++ apps/nextjs/db/migrations/20260101203000_alter_userevent_usuario_id_nullable.ts
@@ -1,3 +1,4 @@
+import { sql } from 'kysely'
 import type { Kysely } from 'kysely'
 
 export async function up(db: Kysely<any>): Promise<void> {
@@ -6,6 +7,10 @@ export async function up(db: Kysely<any>): Promise<void> {
     .alterTable('userevent')
     .alterColumn('usuario_id', (col) => col.dropNotNull())
     .execute()
+  const up = await sql<any>`
+      UPDATE guide_usuario SET created_at = NOW() WHERE created_at IS NULL; 
+    `.execute(db)
+  console.log("up=", up)
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
