418cde3da14401b784c2ff18a7992f375969396b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2026-01-21 16:18:27 +0000

    Trying #76

--- apps/nextjs/components/ui/card.tsx
+++ apps/nextjs/components/ui/card.tsx
@@ -65,7 +65,7 @@ function CardContent({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="card-content"
-      className={cn('px-6', className)}
+      className={cn('flex-1 px-6', className)}
       {...props}
     />
   )
