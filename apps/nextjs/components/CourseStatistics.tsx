862bfc6bcd7e80c55efd16555d8e8a9fdda4e23c
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2026-02-16 21:02:37 +0000

    Removing 00 in statistics

--- apps/nextjs/components/CourseStatistics.tsx
+++ apps/nextjs/components/CourseStatistics.tsx
@@ -33,82 +33,83 @@ export function CourseStatistics({
 }: CourseStatisticsProps) {
   return (
     <div className="flex justify-between items-center p-4 mt-auto">
-      <div>{
-        (scholarshipPerGuide && scholarshipPerGuide > 0 && address && 
-         profileScore && profileScore > 0) ? (
-          <div className="p-2">
-            <span>
-              {lang === 'es'
-                ? 'Beca de '
-                : 'Scholarship of '}$
-              {(
-                (scholarshipPerGuide * 100) /
-                profileScore
-              ).toFixed(2)}{' '}
-              USDT
-              {lang === 'es' ? ' por guía.' : ' per guide.'}
-            </span>
-          </div>
-        ) : (
-          <div className="p-2">
-            <span>
-              {lang === 'es'
-                ? 'Beca de hasta 1 USDT después de que completes tu perfil.'
-                : 'Scholarship of up to 1 USDT after you complete your profile.'}
-            </span>
-          </div>
-        )
-      }
-      { scholarshipPerGuide && scholarshipPerGuide > 0 && address && 
-        percentagePaid && percentagePaid < 100 && !canSubmit && (
-          <div className="p-2">
-            <span className="text-red-500">
-              {lang === 'es'
-                ? 'Aunque estás en etapa de enfriamiento'
-                : 'Although you are in cooldown period.'}
-            </span>
-          </div>
-        )
-      }
-      { scholarshipPerGuide && scholarshipPerGuide > 0 && canSubmit && 
-        percentagePaid && percentagePaid < 100 && (
-          <div className="p-2 text-green-600">
-            <span className="text-green-600">
-              {lang === 'es'
-                ? 'Eres elegible.'
-                : 'You are eligible.'}
-            </span>
+      <div>
+        {
+          (scholarshipPerGuide && scholarshipPerGuide > 0 && address &&
+            profileScore && profileScore > 0) ? (
+            <div className="p-2">
+              <span>
+                {lang === 'es'
+                  ? 'Beca de '
+                  : 'Scholarship of '}$
+                {(                
+                  (scholarshipPerGuide * 100) /
+                  profileScore
+                ).toFixed(2)}{' '}
+                USDT
+                {lang === 'es' ? ' por guía.' : ' per guide.'}
+              </span>
+            </div>
+          ) : (
+            <div className="p-2">
+              <span>
+                {lang === 'es'
+                  ? 'Beca de hasta 1 USDT después de que completes tu perfil.'
+                  : 'Scholarship of up to 1 USDT after you complete your profile.'}
+              </span>
+            </div>
+          )
+        }
+        {scholarshipPerGuide && scholarshipPerGuide > 0 && address &&
+          percentagePaid && percentagePaid < 100 && !canSubmit && (
+            <div className="p-2">
+              <span className="text-red-500">
+                {lang === 'es'
+                  ? 'Aunque estás en etapa de enfriamiento'
+                  : 'Although you are in cooldown period.'}
+              </span>
+            </div>
+          )
+        }
+        {scholarshipPerGuide && scholarshipPerGuide > 0 && canSubmit &&
+          percentagePaid && percentagePaid < 100 && (
+            <div className="p-2 text-green-600">
+              <span className="text-green-600">
+                {lang === 'es'
+                  ? 'Eres elegible.'
+                  : 'You are eligible.'}
+              </span>
+            </div>
+          )}
+        {full && (
+          <div className="pt-2">
+            {lang === 'es' ? 'Total de guias en el curso: ' :
+              'Total number of guides in the course: '}
+            {totalGuides}
           </div>
         )}
-      { full && (
-        <div className="pt-2">
-          {lang === 'es' ? 'Total de guias en el curso: ' : 
-            'Total number of guides in the course: '}
-          {totalGuides}
-        </div>
-      )}
-      { full && completedGuides && percentageCompleted && (
-        <div>
-          {lang === 'es' ? 'Guías aprobadas: ' : 
-            'Approved guides: '}
-          {completedGuides}  ({percentageCompleted}%)
-        </div>
-      )}
-      { full && paidGuides && percentagePaid && (
-        <div>
-          {lang === 'es' ? 'Guías aprobadas con beca pagada: ' : 
-            'Approved guides with scholarship paid: '}
-          {paidGuides} ({percentagePaid}%)
-        </div>
-      )}
-      { full && scholarshipPaid && (
-        <div>
-          {lang === 'es' ? 'Beca total pagada en este curso: ' :
-            'Total scholarship paid in this course: '
-          }
-          {scholarshipPaid} USDT
-        </div>
-      )}
+        {full && typeof completedGuides === 'number' && typeof percentageCompleted === 'number' && (
+          <div>
+            {lang === 'es' ? 'Guías aprobadas: ' :
+              'Approved guides: '}
+            {completedGuides}  ({percentageCompleted}%)
+          </div>
+        )}
+        {full && typeof paidGuides === 'number' && typeof percentagePaid === 'number' && (
+          <div>
+            {lang === 'es' ? 'Guías aprobadas con beca pagada: ' :
+              'Approved guides with scholarship paid: '}
+            {paidGuides} ({percentagePaid}%)
+          </div>
+        )}
+        {full && typeof scholarshipPaid === 'number' && (
+          <div>
+            {lang === 'es' ? 'Beca total pagada en este curso: ' :
+              'Total scholarship paid in this course: ' 
+            }
+            {scholarshipPaid.toFixed(2)} USDT
+          </div>
+        )}
       </div>
       <CompletedProgress
         percentageCompleted={percentageCompleted || 0}
