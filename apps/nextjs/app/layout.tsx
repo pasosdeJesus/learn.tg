498a632c69795f17a16eececc2fbf6e8548e168e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2026-03-31 13:23:47 -0500

    Register with talent

--- apps/nextjs/app/layout.tsx
+++ apps/nextjs/app/layout.tsx
@@ -32,6 +32,9 @@ export default function RootLayout({
       lang="en"
       className={`${dmSans.variable} ${dmMono.variable} antialiased`}
     >
+      <head>
+        <meta name="talentapp:project_verification" content="c14dfa236e1c988ed306bf6b45d21e62e8eebfd25a926b942edbd0ff8111ddfefd7f3c9b7c9e35f4988e8d4d5185e4ff783e51f9fefb850ba9900a1fe1fc9bb0"/>
+      </head>
       <body>
 	<RootLayoutClient>{children}</RootLayoutClient>
       </body>
