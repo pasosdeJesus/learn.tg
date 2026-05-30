e442dddf893091f8fec4bd33846fb1a32a096bee
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2026-05-30 08:26:54 -0400

    siwe.address checksummed

--- apps/nextjs/app/[lang]/[pathPrefix]/[pathSuffix]/test/page.tsx
+++ apps/nextjs/app/[lang]/[pathPrefix]/[pathSuffix]/test/page.tsx
@@ -476,11 +476,6 @@ export default function Page({
                       🎓 {t('credentialEarned')}
                     </div>
                   )}
-                  {credentialEarned && (
-                    <div className="p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800">
-                      🎓 {t('credentialEarned')}
-                    </div>
-                  )}
                   {scholarshipTx && (
                     <div className="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800 break-all">
                       <a
