779210d13260ca157cd6b80f93062549116d3181
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2026-07-03 10:34:57 -0400

    launch

--- apps/nextjs/app/[lang]/page.tsx
+++ apps/nextjs/app/[lang]/page.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image'
 
 import { CourseStatistics } from '@/components/CourseStatistics'
 import { CourseDonation } from '@/components/CourseDonation'
-import { SlearnInfo } from '@pasosdejesus/m/blockchain'
+import { SlearnInfo } from '@pasosdejesus/m/blockchain/SlearnInfo'
 import { AddSlearnButton } from '@/components/AddSlearnButton'
 import { CompletedProgress } from '@/components/ui/completed-progress'
 
